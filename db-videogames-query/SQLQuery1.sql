d5a2d59134a21bcf4d3e222c129f59fd55592122
giovannitamburini <giovatambu@gmail.com>
2023-09-20 16:41:09 +0200

    added querys of 'group by' section

--- db-videogames-query/SQLQuery1.sql
+++ db-videogames-query/SQLQuery1.sql
@@ -89,3 +89,46 @@ WHERE rating = 5;
 SELECT country, COUNT(id) as software_house_number
 FROM software_houses
 GROUP BY country;
+
+/*
+2- Contare quante recensioni ha ricevuto ogni videogioco (del videogioco vogliamo solo l'ID) (500)
+*/
+
+SELECT videogame_id, COUNT(id) AS rewiews_number
+FROM reviews
+GROUP BY videogame_id
+ORDER BY videogame_id ASC;
+
+/*
+3- Contare quanti videogiochi hanno ciascuna classificazione PEGI (della classificazione PEGI vogliamo solo l'ID) (13)
+*/
+
+SELECT pegi_label_id, COUNT(videogame_id) AS videogames_number
+FROM pegi_label_videogame
+GROUP BY pegi_label_id
+ORDER BY videogames_number DESC;
+
+/*
+4- Mostrare il numero di videogiochi rilasciati ogni anno (11)
+*/
+
+SELECT YEAR(release_date), COUNT(id) AS number_of_videogames
+FROM videogames
+GROUP BY YEAR(release_date);
+
+/*
+5- Contare quanti videogiochi sono disponbiili per ciascun device (del device vogliamo solo l'ID) (7)
+*/
+
+SELECT device_id, COUNT(videogame_id) AS number_of_videogames
+FROM device_videogame
+GROUP BY device_id;
+
+/*
+6- Ordinare i videogame in base alla media delle recensioni (del videogioco vogliamo solo l'ID) (500)
+*/
+
+SELECT videogame_id, AVG(rating) as avarage_rating
+FROM reviews
+GROUP BY videogame_id
+ORDER BY avarage_rating DESC;
